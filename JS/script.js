ebe312c4299fdb26f769b2f07b05fe1d503cadc6
ladwongproject <alexladwong@gmail.com>
2023-08-05 22:57:14 +0300

    NewPage

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,3 @@
-// const triggerOpen = document.querySelectorAll('[trigger-button]');
-// const triggerClose = document.querySelectorAll('[close-button]');
-// const overlay = document.querySelector('[data-overlay]');
-
-// for (let i = 0; i < triggerOpen.length; i++) {
-//     let currentId = triggerOpen[i].dataset.target,
-//         targetEl = document.querySelector(`#${currentId}`)
-
-//     const openData = function () {
-//         targetEl.classList.remove('active');
-//         overlay.classList.remove('active');
-//     };
-//     triggerOpen[i].addEventListener('click', function () {
-//         targetEl.classList.add('active');
-//         overlay.classList.add('active');
-//     });
-
-//     targetEl.querySelector('[close-button]').addEventListener('click', openData);
-//     overlay.addEventListener('click', openData);
-// }
-
 
 const triggerOpen = document.querySelectorAll('[trigger-button]');
 const triggerClose = document.querySelectorAll('[close-button]');
@@ -38,6 +17,97 @@ for (let i = 0; i < triggerOpen.length; i++) {
         overlay.classList.add('active');
     });
 
-    targetEl.querySelector('[data-close-button]').addEventListener('click', openData);
+    targetEl.querySelector('[close-button]').addEventListener('click', openData);
     overlay.addEventListener('click', openData);
 }
+
+// Mobile Settings
+const submenu = document.querySelectorAll('.child-trigger');
+submenu.forEach((menu) => menu.addEventListener('click', function (e) {
+    e.preventDefault();
+    submenu.forEach((item) => item != this ? item.closest('.has-Child').classList.remove('active') : null);
+    if (!this.closest('.has-Child').classList.contains('active')) {
+        this.closest('.has-Child').classList.toggle('active');
+    }
+}));
+
+// Sorters
+const sorter = document.querySelector('.sort-list');
+if (sorter) {
+    const sortLi = sorter.querySelectorAll('li'); // Use querySelectorAll() to get all <li> elements
+    sorter.querySelector('.opt-trigger').addEventListener('click', function () {
+        sorter.querySelector('ul').classList.toggle('show');
+    });
+
+    sortLi.forEach((item) => item.addEventListener('click', function () {
+        sortLi.forEach((li) => li != this ? li.classList.remove('active') : null);
+
+        this.classList.add('active');
+        sorter.querySelector('.opt-trigger span.value').textContent = this.textContent;
+        sorter.querySelector('ul').classList.toggle('show');
+    }));
+}
+
+
+
+// Swipper/Slider
+const swiper = new Swiper('.sliderbox', {
+
+    loop: true,
+    effect: 'slide',
+    autoHeight: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+
+// Carousel
+const carousel = new Swiper('.carouselbox', {
+    spaceBetween: 30,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        481: {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+            centeredSlides: false,
+        },
+        640: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+            centeredSlides: false,
+        },
+        995: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+            centeredSlides: false,
+        }
+    }
+});
+
+// Tabbed navigation
+const trigger = document.querySelectorAll('.tabbed-trigger');
+content = document.querySelectorAll('.tabbed > div');
+trigger.forEach((btn) => {
+    btn.addEventListener('click', function () {
+        let dataTarget = this.dataset.id,
+            body = document.querySelector(`${dataTarget}`)
+
+        trigger.forEach((b) => b.parentNode.classList.remove('active'));
+        trigger.forEach((s) => s.classList.remove('active'));
+        this.parentNode.classList.add('active');
+        body.classList.add('active');
+    })
+})
+
+
+
+
